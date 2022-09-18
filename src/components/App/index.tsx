2babe2ea2c63948c1c3bbbaa0687bdcba38f9243
simone-alcala <simone.alcala@gmail.com>
2022-09-18 11:19:58 -0300

    Testing routes

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -4,9 +4,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AuthProvider from '../../contexts/AuthProvider';
 
 import PrivateRoute from '../PrivateRoute';
-import Home from '../../pages/Home';
-import SignIn from '../../pages/SignIn';
-import SignUp from '../../pages/SignUp';
+import Home from './../../pages/Home';
+import SignIn from './../../pages/SignIn';
+import SignUp from './../../pages/SignUp';
 
 function App() {
   return (
